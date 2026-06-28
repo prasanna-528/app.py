42938e08d108c052bb73a39c607f4f83e1683444
prasanna-528 <prasanna51214@gmail.com>
2026-06-28 14:45:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,130 @@
+# -------------------------------
+# Happy Birthday Akhil Rahul ❤️
+# -------------------------------
+
+print("Initializing Birthday Program...")
+print("Please Wait...")
+print()
+
+# Name Verification
+while True:
+    name = input("Enter your Name : ").strip()
+
+    if name == "Akhil Rahul":
+        print()
+        print("Identity Verified ❤️")
+        print("Welcome", name)
+        break
+
+    print("Wrong Name 😂")
+    print("Try Again...\n")
+
+print()
+print("Happy Birthday", name)
+print()
+
+print("Before I continue...")
+print("I want to tell you something.")
+print()
+
+print("You told me many times...")
+print("'Learn Python' ani.")
+
+print()
+
+print("Every single time...")
+print("I said...")
+print("'Tomorrow nerchukunta' 😂")
+
+print()
+
+print("But finally...")
+print("I listened.")
+
+print()
+
+print("Because...")
+print("I wanted my first Python program")
+print("to be only for you.")
+
+print()
+
+def birthday():
+    print("Happy Birthday Akhil Rahul ❤️")
+    print("I wish you all the happiness in the world.")
+    print("Keep smiling 😊")
+    print("Stay happy.")
+    print("Take care of yourself 🫂")
+
+birthday()
+
+print()
+
+# Love Verification
+while True:
+    answer = input("Do you really love me? (yes/no) : ").strip().lower()
+
+    if answer == "yes":
+        break
+
+    print()
+    print("Hmm... 😂")
+    print("I don't believe that answer.")
+    print("Try Again...\n")
+
+# YES vachina tarvata matrame ee story start
+
+print()
+
+print("I knew your answer... ❤️")
+print()
+
+print("I just want to tell you one thing...")
+
+print()
+
+print("Nuvvu ante naku chala istam.")
+print("I Love You So Much ❤️")
+
+print()
+
+print("And...")
+
+print("I'm Sorry...")
+
+print("Ninnu roju yedipistanu...")
+print("Roju visigistanu...")
+print("Roju allari chestanu... 😂")
+
+print()
+
+print("Kani...")
+print("Adantha oka reason valle...")
+
+print()
+
+print("Endukante...")
+print("Nuvvu ante naku chala chala istam. ❤️")
+
+print()
+
+print("Just one promise...")
+print("Please stay with me forever. ❤️")
+
+print()
+
+print("One Last Thing...")
+
+print("Thank you for coming into my life.")
+
+print("You are one of the best things")
+print("that ever happened to me.")
+
+print()
+
+print("Happy Birthday Akhil Rahul ❤️")
+
+print("Stay Happy...")
+print("Stay Healthy...")
+print("And Please...")
+print("Stay Mine Forever... ❤️")
